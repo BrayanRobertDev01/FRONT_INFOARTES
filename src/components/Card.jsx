f8455bd13fc8937e2131908215ed3f4dea77a028
mateus <mateusjmj92@gmail.com>
2024-04-25 00:14:37 -0300

    feat: Reestruturando CSS do projeto

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,24 @@ import "../styles/Card.scss";
 import { RiPencilFill } from "react-icons/ri";
 import { FaTrash } from "react-icons/fa";
 
-export default function Card({ properties, quantity, value, cor}) {
+export default function Card({ properties, quantity, value, cor }) {
   return (
-    <div className="cardcontainer">
-      <div className="headcards">
-        <div className="headicons">
-        <RiPencilFill />
-        <FaTrash />
+    <>
+      <div className="mainCard">
+        <header>
+          <h1>{properties}</h1>
+        </header>
+        <div style={{ display: 'flex', gap: '15%', padding: '0 10px 0 10px', fontSize: 18 }}>
+          {quantity ? (<label>Quantidade: {quantity}</label>) : null}
+          {cor ? (<label>Cor: {cor}</label>) : null}
+          <label>Valor: R${value},00</label>
         </div>
-        <h1>{properties}</h1>
+        <section className="headicons">
+          <RiPencilFill />
+          <FaTrash />
+        </section>
       </div>
-      <div className="bodycards">
-        {quantity ? (<label>Quantidade: {quantity}</label>) : null}
-        {cor ? (<label>Cor: {cor}</label>) : null}
-        <label>Valor: R${value},00</label>
-      </div>
-    </div>
+    </>
   );
 }
 
